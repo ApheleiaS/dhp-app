530faec8a6610babb376ef99e79d43a6b4298ae5
ApheleiaS <akanksha.dlf@gmail.com>
2018-08-25 20:33:15 +0530

    concurrency in app

--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
-from flask import render_template, flash, redirect, request, url_for
+from flask import render_template, flash, redirect, request, url_for, session
 from app import app, db
 from .forms import ParticipantForm
 from .models import User, Answer
 
-curent_user = 0  # User number who has logged in
+curent_user = []  # User number who has logged in
 logged_in = 0  # Set when someone is logged in
 
-session = {"pid": 0, "username": ""}
+#session = {"pid": 0, "username": ""}
 length_of_survey = 10  # Number of questions here
 files_for_survey = [
     "../static/image_1/out_A_24_1.jpg", "../static/image_1/out2.jpg",
@@ -16,10 +16,15 @@ files_for_survey = [
     "../static/image_1/out10.jpg", "../static/image_1/out12.jpg"]
 
 
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    session['pid'] = 0
-    session['username'] = ""
+    if "user_id" in session:
+        if session["curr_question"]:
+            return redirect('/survey/'+session["curr_question"])
+        else:
+            return redirect('/index')
+
     form = ParticipantForm()
     if form.validate_on_submit():
 
@@ -27,16 +32,21 @@ def login():
         p = User(form.username.data, form.email.data, form.age.data,
                  form.eyesightl.data, form.eyesightr.data, form.gender.data,
                  form.profession.data)
-        db.session.add(p)
-        db.session.commit()
-        pid = p.id
-        uname = p.username
+        try:
+            db.session.add(p)
+            db.session.commit()
+            pid = p.id
+            uname = p.username
+            
+            # Start session with pid and uname from DB
+            session["user_id"] = pid
+            print session
+            return redirect('/index')
 
-        # Start session with pid and uname from DB
-        session["pid"] = pid
-        session["username"] = uname
+        except IntegrityError:
+            flash('The username or email is already used please login again!')
 
-        return redirect('/index')
+
     return render_template('login.html',
                            title='Sign In',
                            form=form)
@@ -45,23 +55,20 @@ def login():
 # put logout button on every page of the survey
 @app.route('/logout')
 def logout():
-    session['pid'] = 0
-    session['username'] = ""
+    session.pop("user_id")
     # print (session)
     return redirect('/login')
 
 
 @app.route('/finished_survey/<pid>')
 def finished_survey(pid):
-    session['pid'] = 0
-    session['username'] = ""
+    session.pop("user_id")
     return render_template("completed_survey.html", pid=pid)
 
 # This page first collects favorite color data and gives instructions for THE SURVEY
 # and then asks you to begin the survey.
 
 
-@app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
 
@@ -74,32 +81,30 @@ def index():
   # db.session.commit()
   #       redirect(url_for(survey, question_num="1"))
     message = "Hello"
-    if session["pid"] and session["username"]:
+    if "user_id" in session:
+        print "in session now"
         return render_template(
             'index.html', message=message, title="Survey Home",
-            username=session['username'], pid=session['pid'])
+            pid=session.get("user_id"))
     else:
         return redirect('login')
 
 
 @app.route('/survey/<question_num>', methods=['GET', 'POST'])
 def survey(question_num):
-    if session['pid'] == 0 or session['username'] == "":
-        return redirect(url_for('login'))
-
+    if "user_id" not in session:
+        return redirect('login')
+    user = User.query.get(session.get("user_id")).id
     image = files_for_survey[int(question_num) - 1]
-
     next_ques = str(int(question_num) + 1)
-
     if request.method == "POST":
         hue = request.form.get('hue')
         sat = request.form.get('sat')
         light = request.form.get('light')
-        dominant_color_chosen = "hue:" + \
-            hue + ",sat:" + sat + ",light:" + light
+        dominant_color_chosen = "hue:" + hue + ",sat:" + sat + ",light:" + light
         # Answers(userid, surveynum(1,2,3), questionnum(1-10), answerstring)
-        ans = Answer(session['pid'], int(
-            question_num), image + "," + dominant_color_chosen)
+        ans = Answer(user, int(question_num), image + "," + dominant_color_chosen)
+        print ans
         db.session.add(ans)
         db.session.commit()
 
@@ -107,7 +112,7 @@ def survey(question_num):
             # Update User Info with completion of survey
             # participant = User.query.get(session['pid'])
             # db.session.commit()
-            return redirect(url_for('finished_survey', pid=session['pid']))
+            return redirect(url_for('finished_survey', pid=session['user_id']))
         else:
             return redirect(url_for('survey', question_num=next_ques))
 
--- config.py
+++ config.py
@@ -5,4 +5,5 @@ DEBUG = False
 TESTING = False
 CSRF_ENABLED = True
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-SECRET_KEY = 'this-really-needs-to-be-changed'
+SECRET_KEY = 'thisisdfjdshkddfjchanged'
+CSRF_SESSION_KEY = 'ad190242b1dd440584ab5324688526dshb'
