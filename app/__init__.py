f2ad2a3e273340a72d3a81a6368ce9d2896e1b7d
ApheleiaS <akanksha.dlf@gmail.com>
2018-08-27 03:07:22 +0530

    added logging to __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
+import logging
+import sys
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 app.config.from_object('config')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
